c77a4972475138520ecf085896f39c3dd84e3dfa
Neamar <neamar@neamar.fr>
2014-10-02 15:11:01 +0200

    Only display hour for timespan with similar dates

--- scripts/index.js
+++ scripts/index.js
@@ -9,23 +9,43 @@ window.snippetStyle = {
     options = options || {};
     moment.locale(options.locale || 'en');
     var doc = options.document || document;
+
+    // Format .anyfetch-date item
     var dateNodes = doc.getElementsByClassName('anyfetch-date');
-    for(var i = 0; i < dateNodes.length; i += 1) {
-      var node = dateNodes[i];
+    Array.prototype.forEach.call(dateNodes, function(node) {
       var iso8601 = node.getAttribute("data-iso8601");
+
       if(!iso8601) {
         iso8601 = node.textContent;
         node.setAttribute("data-iso8601", iso8601);
       }
       var mDate = moment(iso8601);
       var formatted;
+
       if(options.fromNow) {
         formatted = mDate.fromNow();
       } else {
-        formatted = mDate.format(options.format || 'llll');
+        formatted = mDate.format(options.format || 'lll');
       }
       node.textContent = formatted.charAt(0).toUpperCase() + formatted.slice(1);
-    }
+    });
+
+    // Format .anyfetch-date in .anyfetch-date-span
+    var dateSpanNodes = doc.getElementsByClassName('anyfetch-date-span');
+    Array.prototype.forEach.call(dateSpanNodes, function(node) {
+      var dates = node.getElementsByClassName('anyfetch-date');
+      var firstTime = dates[0];
+      var secondTime = dates[1];
+      if(firstTime && secondTime) {
+        var firstDate = moment($(firstTime).attr('data-iso8601'));
+        var secondDate = moment($(secondTime).attr('data-iso8601'));
+
+        if(firstDate.format('L') === secondDate.format('L')) {
+          // Same date, let's keep only the hour
+          secondTime.textContent = secondDate.format('LT');
+        }
+      }
+    });
   }
 };
 
