b4fa3742cd52862963400cc35274a30ded2ddc31
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-11-18 14:27:10 +0100

    Gulpfile refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+"use strict";
+/* jshint node: true */
+
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
@@ -8,70 +11,85 @@ var debug = require('gulp-debug');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 
-var LESS  = './stylesheets/style.less';
-var JS   = './scripts/';
-var IMG   = './images/';
-var DEST = './dist/';
-var BOWER = './bower_components/';
-var MOMENT = BOWER + 'moment/min/';
+var paths = {
+  less: {
+    path: './stylesheets',
+    entryPoint: './stylesheets/style.less'
+  },
+  js: {
+    path: './scripts/',
+  },
+  img: {
+    path: './images/',
+  },
+  dest: {
+    path: './dist/',
+  },
+  moment: {
+    path: './bower_components/moment/min/'
+  }
+};
 
 gulp.task('less.normal', function() {
-  return gulp.src(LESS)
+  return gulp.src(paths.less.entryPoint)
     .pipe(less())
     .pipe(rename({
       basename: 'index',
       extname: '.css'
     }))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(paths.dest.path));
 });
 
 gulp.task('less.min', function() {
-  return gulp.src(LESS)
+  return gulp.src(paths.less.entryPoint)
     .pipe(less())
     .pipe(minifyCss())
     .pipe(rename({
       basename: 'index',
       extname: '.min.css'
     }))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(paths.dest.path));
 });
 
 gulp.task('less', ['less.normal', 'less.min']);
 
 gulp.task('lint', function() {
-  return gulp.src(JS + '*.js')
+  return gulp.src(paths.js.path + '*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('minifyJs', function minifyJs() {
-  return gulp.src(JS + '*.js')
-    .pipe(gulp.dest(DEST))
+  return gulp.src(paths.js.path + '*.js')
+    .pipe(gulp.dest(paths.dest.path))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(paths.dest.path));
 });
 
 gulp.task('concatJs', ['minifyJs'], function concatCss() {
-  return gulp.src([MOMENT + 'moment-with-locales.min.js', DEST + 'index.min.js'])
+  return gulp.src([paths.moment.path + 'moment-with-locales.min.js', paths.dest.path + 'index.min.js'])
     .pipe(concat('index-moment.min.js'))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(paths.dest.path));
 });
 
 gulp.task('svgProviders', function svgProviders() {
-  return gulp.src(IMG + 'providers/*.svg')
+  return gulp.src(paths.img.path + 'providers/*.svg')
     .pipe(svgmin([
       { convertPathData: false },
     ]))
-    .pipe(gulp.dest(DEST + "images/providers/"));
+    .pipe(gulp.dest(paths.dest.path + "images/providers/"));
 });
 
 gulp.task('img', ['svgProviders']);
+gulp.task('js', ['minifyJs', 'concatJs']);
 
-gulp.task('watch', function watch() {
-  gulp.watch('./**/*', ['build']);
+gulp.task('watch', ['build'], function watch() {
+  gulp.watch(paths.img.path, ['img']);
+  gulp.watch(paths.js.path, ['js']);
+  gulp.watch(paths.less.path, ['less']);
 });
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['less', 'lint', 'minifyJs', 'concatJs', 'img']);
+gulp.task('build', ['less', 'lint', 'js', 'img']);
